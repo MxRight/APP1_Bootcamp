d2c89be6403b732da599c240a4acc147f2219bbd
Renatann <maxim_n@bk.ru>
2025-10-08 13:41:19 +0300

    add task1-3

---
+++ AP1-Py-T01/task3.py
@@ -0,0 +1,26 @@
+def main():
+    return figure_finder(read_file())
+
+
+def read_file(filename='input.txt'):
+    res = []
+    with open(filename, 'r', encoding='utf-8') as input_f:
+        for line in input_f:
+            res.append(list(map(int, line.split())))
+    return res
+
+
+def figure_finder(matrix):
+    square, circle = 0, 0
+    visited = set()
+    for i, row in enumerate(matrix):
+        for j, element in enumerate(row):
+            if element == 1:
+                visited.add((i, j))
+    return visited
+    #return square, circle
+
+
+if __name__ == "__main__":
+    print(main())
+    # main()
