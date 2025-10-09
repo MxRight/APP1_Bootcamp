338de0212543bff10e971917d471892568946f6f
Renatann <maxim_n@bk.ru>
2025-10-09 19:06:16 +0300

    rm files

--- AP1-Py-T01/task1.py
+++
@@ -1,7 +0,0 @@
-def vector_mul():
-    x1, y1, z1 = map(float, input().split())
-    x2, y2, z2 = map(float, input().split())
-    return x1*x2 + y1*y2 + z1*z2
-
-if __name__ == "__main__":
-    print(vector_mul())
