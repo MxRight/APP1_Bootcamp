9e3fbdda386f58c974b83495c421361178c54080
Renatann <maxim_n@bk.ru>
2025-10-10 12:29:57 +0300

    fix func

--- AP1-Py-T01/src/exercise5/task5.py
+++ AP1-Py-T01/src/exercise5/task5.py
@@ -18,6 +18,7 @@ def validation(num: str):
 
     point_flag = 0
     minus_flag = 0
+    numbers_after_point = None
     stop_flag = False
     allow_numbers_str = map(str, range(0, 10))
     integer_part, fractional_part = [], []
@@ -27,11 +28,14 @@ def validation(num: str):
             minus_flag += 1
         elif i == '.':
             point_flag += 1
+            numbers_after_point = 0
         elif i in allow_numbers_str:
             result.append(i)
+            if point_flag == 1:
+                numbers_after_point+=1
         else:
             stop_flag = True
-        if stop_flag or minus_flag > 1 or point_flag > 1:
+        if stop_flag or minus_flag > 1 or point_flag > 1 or numbers_after_point == 0:
             return 'Conversion error!'
 
 
