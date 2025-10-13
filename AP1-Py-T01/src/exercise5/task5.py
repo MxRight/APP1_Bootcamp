05fefffa4ffd653140bf090c8dca9f79c34102e5
Renatann <maxim_n@bk.ru>
2025-10-13 12:32:17 +0300

    add task5

--- AP1-Py-T01/src/exercise5/task5.py
+++ AP1-Py-T01/src/exercise5/task5.py
@@ -1,44 +1,57 @@
-def my_float():
+from math import pow
+
+
+def validation():
     num = input()
     len_of_num = len(num)
-    if len_of_num > 0:
-        return validation(num)
-    else:
-        return 'Conversion error!'
-
-
-def validation(num: str):
-    """
-    Не пустая.
-    Может содержать только: цифры, один минус (только в начале), и не более одной точки.
-    После минуса должно быть хотя бы одна цифра или точка с цифрами.
-    После точки тоже должна быть хотя бы одна цифра, если она есть.
-
-    """
-
-    point_flag = 0
-    minus_flag = 0
-    numbers_after_point = None
-    stop_flag = False
-    allow_numbers_str = map(str, range(0, 10))
-    integer_part, fractional_part = [], []
-    result = []
+    error_msg = 'Conversion error!'
+    if len_of_num == 0:
+        return error_msg
+    is_valid = True
+    point_flag = False
+    minus_flag = False
+    numbers_of_elements = 0
+    numbers_after_point = 0
+    allow_numbers_str = list(map(str, range(0, 10)))
+    str_integer_lst = []
     for i in num:
-        if i == '-':
-            minus_flag += 1
-        elif i == '.':
-            point_flag += 1
-            numbers_after_point = 0
+        if i == '-' and minus_flag == False and numbers_of_elements == 0:
+            minus_flag = True
+        elif i == '-' and (minus_flag == True or numbers_of_elements > 0):
+            is_valid = False
+            break
+        elif i == '.' and point_flag == True:
+            is_valid = False
+            break
+        elif i == '.' and point_flag == False:
+            point_flag = True
         elif i in allow_numbers_str:
-            result.append(i)
-            if point_flag == 1:
-                numbers_after_point+=1
+            str_integer_lst.append(i)
+            if point_flag:
+                numbers_after_point += 1
         else:
-            stop_flag = True
-        if stop_flag or minus_flag > 1 or point_flag > 1 or numbers_after_point == 0:
-            return 'Conversion error!'
+            is_valid = False
+            break
+        numbers_of_elements += 1
+    if point_flag and numbers_after_point == 0:
+        is_valid = False
+    if is_valid:
+        return calculation(str_integer_lst, numbers_after_point, minus_flag)
+    else:
+        return error_msg
 
+
+def calculation(str_integer_lst, numbers_after_point, minus=False):
+    result = 0
+    for k, v in enumerate(str_integer_lst[::-1]):
+        result += int(v) * pow(10, k)
+    if numbers_after_point > 0:
+        result /= pow(10, numbers_after_point)
+    if minus:
+        result *= -1
+    return result * 2
 
 
 if __name__ == "__main__":
-    my_float()
+    res = validation()
+    print(res if isinstance(res, str) else f"{res:.3f}")
