6f893e612abb12729c2e6b00d1b0a2e8192f370e
Maxim N. <88425424+MxRight@users.noreply.github.com>
2025-11-07 09:47:00 +0300

    Update task1.py

--- AP1-Py-T02/src/exercise1/task1.py
+++ AP1-Py-T02/src/exercise1/task1.py
@@ -1,13 +1,13 @@
 import time
-from multiprocessing import Process, Queue
+from multiprocessing import Process, Queue, Event, Manager
 import queue
 import random
 import os
 import sys
 from prettytable import PrettyTable
+from functools import wraps
+from io import StringIO
 
-
-# реализовать перерывы экзаменаторов
 
 class Man:
     def __init__(self, name: str, gender: str):
@@ -24,15 +24,12 @@ class Student(Man):
         self.waiting = True
         self.succes = None
 
-    def choosing_answer(self, male=True):
-        pass
-
 
 class Examiner(Man):
     def __init__(self, name: str, gender: str):
         super().__init__(name, gender)
-        # self.queue = Queue()  # неправильно, если сформировать очередь сразу, но и ограничить очередь одним студентом, тоже?
-        self.mood = self.set_mood()
+        self.mood = None  # настроение на весь день или на экзамен?
+        self.active = True  # для перерыва
         self.work_time = 0
         self.current_exam = None
         self.all_examined_students = 0
@@ -44,22 +41,6 @@ class Examiner(Man):
         # 1 - плохое настроение
         # 2 - хорошее настроение
         return random.choice(([0, 1, 0, 2, 0, 2, 0, 0]))
-
-    def time_of_exam(self):
-        len_of_name = len(self.name)
-        return random.choice(list(range(len_of_name - 1, len_of_name + 1)))
-
-    def start_exam(self, student: Student):
-        student.waiting = False
-        self.current_exam = student
-        exam_time = self.time_of_exam()
-        self.work_time += exam_time
-        time.sleep(exam_time)
-        # проставляем оценку и отправляем студента домой
-        self.current_exam = None
-
-    def choosing_question(self, male=True):
-        pass
 
 
 class Question:
@@ -80,10 +61,19 @@ class Exam:
     DICT_OF_SELF = {Examiner: "list_of_examiners", Question: "list_of_questions", Student: "list_of_students"}
 
     def __init__(self):
-        self.list_of_examiners = []
-        self.list_of_students = []
-        self.list_of_questions = []
-        self.time_from_start = 0.00
+        manager = Manager()
+
+        self.dict_of_examiners = manager.dict()
+        self.dict_of_students = manager.dict()
+        self.dict_of_questions = manager.dict()
+
+        self.tasks = Queue()
+        self.stop_flag = Event()
+        self.workers = []
+        self.time_start = None
+        self.time_from_start = None
+        self.all_student = None
+        self.all_passed_students = 0
 
     def scan_file(self, class_name, split_text=True):
         with open(self.DICT_OF_FILES[class_name], "r", encoding="utf-8") as f_in:
@@ -93,64 +83,204 @@ class Exam:
                 objects = [class_name(line.strip()) for line in f_in]
         setattr(self, self.DICT_OF_SELF[class_name], objects)
 
-    def load_data(self):
+    def load_data(self): # пока не используем
         for class_name in self.DICT_OF_FILES:
             split_text = class_name is not Question
             self.scan_file(class_name, split_text)
 
-    def start_exam(self):
-        while not self.list_of_students:
-            for examiner in self.list_of_examiners:
-                if not examiner.current_exam:
-                    examiner.start_exam(self.list_of_students.pop())
+    def start_exam(self, examiner_name, student):
+        # Берём копии из manager-словарей
+        student_obj = self.dict_of_students[student]
+        examiner_obj = self.dict_of_examiners[examiner_name]
 
-        # self.list_of_students.close()
+        # Отмечаем: студент сдал документы и зашёл
+        student_obj.waiting = False
+        examiner_obj.current_exam = student
+        self.dict_of_examiners[examiner_name] = examiner_obj
+
+        # Время экзамена зависит от длины имени экзаменатора
+        exam_time = random.randint(len(examiner_name) - 1, len(examiner_name) + 1)
+
+        self.dict_of_examiners[examiner_name] = examiner_obj
+
+        time.sleep(exam_time)
+
+        # Определяем результат (пока используем заглушку)
+        student_obj.succes = random.choice([True, False])
+
+        # Обновляем статистику экзаменатора
+        examiner_obj.current_exam = None
+        examiner_obj.work_time += exam_time
+        examiner_obj.all_examined_students += 1
+        if not student_obj.succes:
+            examiner_obj.all_failed_students += 1
+
+        # Перезаписываем оба объекта обратно в manager
+        self.dict_of_students[student] = student_obj
+        self.dict_of_examiners[examiner_name] = examiner_obj
+
+    def worker(self, examiner_name, stop_flag):
+        while True:
+            try:
+                student = self.tasks.get()
+            except queue.Empty:
+                continue
+            if student is None:
+                break
+            self.start_exam(examiner_name, student)
+
+    def add_task(self, item):
+        self.tasks.put(item)
+
+    def set_elapsed_time(self):
+        self.time_from_start = time.time() - self.time_start
+
+    def start_workers(self):
+        for p in self.workers:
+            p.start()
+
+    def load_demo(self):
+        students = {
+            "Андрей": Student("Андрей", "М"),
+            "Максим": Student("Максим", "М"),
+            "Алексей": Student("Алексей", "М"),
+            "Евгения": Student("Евгения", "Ж"),
+            "Мария": Student("Мария", "Ж"),
+            "Елена": Student("Евгения", "Ж"),
+            "Маргарита": Student("Мария", "Ж"),
+        }
+        examiners = {
+            "Александр": Examiner("Александр", "М"),
+            "Дмитрий": Examiner("Дмитрий", "М"),
+            "Михаил": Examiner("Михаил", "М"),
+        }
+
+        for name, student in students.items():
+            self.dict_of_students[name] = student
+
+        for name, examiner in examiners.items():
+            self.dict_of_examiners[name] = examiner
 
     def start(self):
-        self.load_data()
-        self.start_exam()
+        self.load_demo()
+        self.workers = [
+            Process(target=self.worker, args=(examiner, self.stop_flag))
+            for examiner in self.dict_of_examiners.keys()
+        ]
 
-        self.print_student_table()
-        self.print_examine_table()
+        # заполняем очередь студентами
+        self.all_student = len(self.dict_of_students)
+        for student in self.dict_of_students.keys():
+            self.add_task(student)
+        self.time_start = time.time()
+        self.start_workers()
 
-        print(f'{self.TEXT_OUTPUT_ONE}{0} из {0:.2f}')
-        print(f'{self.TEXT_OUTPUT_TWO}{self.time_from_start}')
+    def stop(self):
+        for _ in self.workers:
+            self.tasks.put(None)
+        self.stop_flag.set()
+        for p in self.workers:
+            p.join()
+
+    def clean_screen(func):
+        last_height = 0
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            nonlocal last_height
+            import contextlib
+            buffer = StringIO()
+            with contextlib.redirect_stdout(buffer):
+                func(*args, **kwargs)
+            output = buffer.getvalue()
+            lines = output.count("\n") + 1
+            if last_height:
+                sys.stdout.write(f"\033[{last_height}F")
+                sys.stdout.write("\033[J")
+
+            sys.stdout.write(output)
+            sys.stdout.flush()
+
+            last_height = lines
+
+        return wrapper
+
+    def print_data(self):
+        self.set_elapsed_time()
+        print(
+            f'\r{self.TEXT_OUTPUT_ONE} {self.tasks.qsize()} из {self.all_student}\n{self.TEXT_OUTPUT_TWO} {self.time_from_start:.2f}')
 
     def print_student_table(self):
         student_table = PrettyTable()
         student_table.field_names = ["Студент", "Статус"]
-        for student in self.list_of_students:
-            if student.waiting:
+        for student_name, student in self.dict_of_students.items():
+            if getattr(student, "waiting", False):
                 status = "Очередь"
+            elif getattr(student, "succes", False):
+                status = "Сдал"
             else:
-                if student.succes:
-                    status = "Сдал"
-                else:
-                    status = "Провалил"
-            student_table.add_row([student.name, status])
+                status = "Провалил"
+
+            student_table.add_row([student_name, status])
+
         print(student_table)
 
-    def print_examine_table(self):
+    def print_examine_table(self, finish=False):
         examiner_table = PrettyTable()
-        examiner_table.field_names = ["Экзаменатор", "Текущий студент", "Всего студентов", "Завалил", "Время работы "]
-        for examiner in self.list_of_examiners:
-            examiner_table.add_row(
-                [examiner.name, examiner.current_exam, examiner.all_examined_students, examiner.all_failed_students,
-                 examiner.work_time])
+        if finish:
+            examiner_table.field_names = [
+                "Экзаменатор",
+                "Всего студентов",
+                "Завалил",
+                "Время работы"
+            ]
+        else:
+
+            examiner_table.field_names = [
+                "Экзаменатор",
+                "Текущий студент",
+                "Всего студентов",
+                "Завалил",
+                "Время работы"
+            ]
+
+        for examiner_name in self.dict_of_examiners.keys():
+            examiner = self.dict_of_examiners[examiner_name]
+
+            if finish:
+                examiner_table.add_row([
+                    examiner.name,
+                    examiner.all_examined_students,
+                    examiner.all_failed_students,
+                    examiner.work_time
+                ])
+            else:
+                current_student = examiner.current_exam if examiner.current_exam is not None else "-"
+
+                examiner_table.add_row([
+                    examiner.name,
+                    current_student,
+                    examiner.all_examined_students,
+                    examiner.all_failed_students,
+                    examiner.work_time
+                ])
+
         print(examiner_table)
 
-    def print_finish_examiner_table(self):
-        pass
-
-    @staticmethod
-    def clear_screen():
-        os.system('cls' if os.name == 'nt' else 'clear')
-
-    def clear_screen2(self, output_data):
-        sys.stdout.write(f'\r{output_data}')
-        sys.stdout.flush()
+    @clean_screen
+    def print_double_tables(self, finish=False):
+        self.print_student_table()
+        print()
+        self.print_examine_table(finish=finish)
+        print()
+        self.print_data()
 
 
 if __name__ == "__main__":
     exam = Exam()
     exam.start()
+    sys.stdout.write("\033[H")
+    while not exam.tasks.empty():
+        time.sleep(0.1)
+        exam.print_double_tables()
+    exam.stop()
+    exam.print_double_tables(finish=True)
