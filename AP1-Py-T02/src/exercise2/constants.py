2583904d484cc77b8727b5ad38da1f1b629edb7d
Renatann <maxim_n@bk.ru>
2025-11-09 16:38:03 +0300

    update VERSION

--- AP1-Py-T02/src/exercise2/constants.py
+++ AP1-Py-T02/src/exercise2/constants.py
@@ -1,6 +1,6 @@
 HEADER_BROWSER = 'Mozilla/5.0'
 MODULE_NAME = 'Async Downloader by Renatann'
-VERSION = '0.8'
+VERSION = '0.9'
 PATH_INPUT_TEXT = "Введите название папки для сохранения файлов, если папки не существует, то она будет создана (по умолчанию используется папка img): "
 URL_INPUT_TEXT = "Для скачивания файлов, вводите ссылки поддерживаются массивы ссылок через пробел, запятую, перенос строки (Enter для выхода): "
 INCORRECT_PATH_ERROR_TEXT = "Некорректный путь!"
