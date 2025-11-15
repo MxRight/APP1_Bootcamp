58f261ee3968aae307b6e5c6bc043426a6b131d9
Renatann <maxim_n@bk.ru>
2025-11-15 18:39:24 +0300

    exec bash -off

--- AP1-Py-P01/src/start.sh
+++ AP1-Py-P01/src/start.sh
@@ -1,1 +1,1 @@
- gnome-terminal --full-screen -- bash -c "source .venv/bin/activate; python3 main.py"
+ gnome-terminal --full-screen -- bash -c "source .venv/bin/activate; python3 main.py; exec bash"
