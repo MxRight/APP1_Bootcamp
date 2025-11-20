7ede5c4523abcfe83caf59d8e7ddebc6abb8f66a
Renatann <maxim_n@bk.ru>
2025-11-20 10:57:58 +0300

    rename class to EnemyRender

--- AP1-Py-P01/src/presentation/render2d/entities/enemies.py
+++ AP1-Py-P01/src/presentation/render2d/entities/enemies.py
@@ -2,16 +2,16 @@ from dataclasses import dataclass
 
 
 @dataclass(frozen=True)
-class EnemyView:
+class EnemyRender:
     symbol: str
     color: str
 
 
 ENEMIES = {
-    "zombie ": EnemyView("z", "green"),
-    "vampire": EnemyView("v", "red"),
-    "ghost": EnemyView("g", "white"),
-    'ogre': EnemyView("o", "yellow"),
-    'snake': EnemyView("s", "white"),
-    'mimic': EnemyView("m", "white")
+    "zombie ": EnemyRender("z", "green"),
+    "vampire": EnemyRender("v", "red"),
+    "ghost": EnemyRender("g", "white"),
+    'ogre': EnemyRender("o", "yellow"),
+    'snake': EnemyRender("s", "white"),
+    'mimic': EnemyRender("m", "white")
 }
