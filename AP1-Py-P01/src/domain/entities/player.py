95c5eb1ecc8a5188de2ce872427a5a2f8be92b44
Renatann <maxim_n@bk.ru>
2025-11-20 11:00:42 +0300

    fix init

--- AP1-Py-P01/src/domain/entities/player.py
+++ AP1-Py-P01/src/domain/entities/player.py
@@ -1,5 +1,6 @@
 from .gameobject import Creature
 from .backpack import Backpack
+from application.constants import PLAYER_CODE
 
 class Player(Creature):
     STARTHP = 100
@@ -7,18 +8,19 @@ class Player(Creature):
     STARTDEXTERITY = 8
     STARTSTRENGTH = 9
 
-    def __init__(self):
-        super().__init__()
+    def __init__(self, name, x, y):
+        super().__init__(name, x, y)
         self.max_health = None
         self.weapon = None
         self.backpack = Backpack()
 
-    def start(self):  # надо сделать универсальной для стартов уровней
+    def start(self):
         self.health = self.STARTHP
         self.max_health = self.MAXSTARTHP
         self.dexterity = self.STARTDEXTERITY
         self.strength = self.STARTSTRENGTH
-        self.drop_in_room(1)
+        self.set_kind(PLAYER_CODE)
+
 
     def dead(self):
         pass
