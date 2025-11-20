43685a5c69132bacfac7404eb2ded81c41e797d2
Renatann <maxim_n@bk.ru>
2025-11-20 10:59:14 +0300

    fix init

--- AP1-Py-P01/src/domain/entities/gameobject.py
+++ AP1-Py-P01/src/domain/entities/gameobject.py
@@ -1,18 +1,22 @@
 class GameObject:
-    def __init__(self):
-        self.x = None
-        self.y = None
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.kind = None
         self.type = None
         self.active = False  # отображаются и действуют только активные объекты
+
+    def set_kind(self, kind):
+        self.kind = kind # устанавливаем слово для интерпретации объекта слоем presentation по заданному словарю
 
     def drop_in_room(self, room_id: int):
         pass
 
 
 class Creature(GameObject):
-    def __init__(self):
-        super().__init__()
-        self.name = None
+    def __init__(self, name, x, y):
+        super().__init__(x, y)
+        self.name = name
         self.health = None
         self.dexterity = None
         self.strength = None
