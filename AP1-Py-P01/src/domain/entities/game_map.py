3bb176bcc1c726aa6292ac1f199977c40c17a149
Renatann <maxim_n@bk.ru>
2025-11-20 10:58:43 +0300

    insert TILE_CODE var

--- AP1-Py-P01/src/domain/entities/game_map.py
+++ AP1-Py-P01/src/domain/entities/game_map.py
@@ -1,10 +1,11 @@
 from .map_tile import Tile
+from application.constants import WALL_TILE_CODE
 
 class GameMap:
     def __init__(self, width, height):
         self.width = width
         self.height = height
-        self.tiles = [[Tile(False, False, "wall") for _ in range(width)] for _ in range(height)] # заполняем всё пространство стенами, позже прорубим комнаты и проходы
+        self.tiles = [[Tile(False, False, WALL_TILE_CODE) for _ in range(width)] for _ in range(height)] # заполняем всё пространство стенами, позже прорубим комнаты и проходы
 
     def is_walkable(self, x, y):
         return self.tiles[y][x].walkable
