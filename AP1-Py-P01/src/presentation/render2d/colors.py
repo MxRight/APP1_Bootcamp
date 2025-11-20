f46db2fe534a5f1136641e09641778b6b0288410
Renatann <maxim_n@bk.ru>
2025-11-20 10:57:20 +0300

    add test color

--- AP1-Py-P01/src/presentation/render2d/colors.py
+++ AP1-Py-P01/src/presentation/render2d/colors.py
@@ -1,11 +1,12 @@
 import curses
 
 COLORS = {
-    "white":  (curses.COLOR_WHITE, curses.COLOR_BLACK),
+    "white": (curses.COLOR_WHITE, curses.COLOR_BLACK),
     "yellow": (curses.COLOR_YELLOW, curses.COLOR_BLACK),
-    "green":  (curses.COLOR_GREEN, curses.COLOR_BLACK),
-    "red":    (curses.COLOR_RED, curses.COLOR_BLACK),
-    "magenta":(curses.COLOR_MAGENTA, curses.COLOR_BLACK),
-    "cyan":   (curses.COLOR_CYAN, curses.COLOR_BLACK),
-    "blue":   (curses.COLOR_BLUE, curses.COLOR_BLACK),
+    "green": (curses.COLOR_GREEN, curses.COLOR_BLACK),
+    "red": (curses.COLOR_RED, curses.COLOR_BLACK),
+    "magenta": (curses.COLOR_MAGENTA, curses.COLOR_BLACK),
+    "cyan": (curses.COLOR_CYAN, curses.COLOR_BLACK),
+    "blue": (curses.COLOR_BLUE, curses.COLOR_BLACK),
+    "yellow_white": (curses.COLOR_YELLOW, curses.COLOR_WHITE),
 }
