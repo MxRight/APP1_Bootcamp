68ed4194d36d7df157ac1bd1dae420701424385e
Renatann <maxim_n@bk.ru>
2025-11-22 14:37:23 +0300

    del old constants

--- AP1-Py-P01/src/game_setting.py
+++ AP1-Py-P01/src/game_setting.py
@@ -4,15 +4,3 @@ MIN_ROOM_W_SIZE = 6
 MAX_ROOM_W_SIZE = 20
 MIN_ROOM_H_SIZE = 6
 MAX_ROOM_H_SIZE = 16
-
-GAME_TITLE = [("ROGUE", "red", "big"), ("REMAKE 2025", "cyan", "small"), ("by Renatann", "green", "small")]
-
-COLORS = {
-    'red': '\033[91m',
-    'cyan': '\033[96m',
-    'reset': '\033[0m',
-    'purple': '\033[35m',
-    'green light': '\033[92m',
-    'yellow': '\033[33m',
-    'green': '\033[32m'
-}
